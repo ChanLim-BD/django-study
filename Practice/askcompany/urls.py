b14e7b7c72d23d868fdc53b578ab44bc1f068171
Chan <linsay070@gmail.com>
2022-06-27 11:53:31 +0900

    migrations-test

--- Practice/askcompany/urls.py
+++ Practice/askcompany/urls.py
@@ -26,6 +26,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('instagram/', include('instagram.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
