8811e0d1c695a05db836ba22e5aa6d40deb0279a
Chan <linsay070@gmail.com>
2022-06-28 22:44:09 +0900

    .

--- Practice/accounts/models.py
+++ Practice/accounts/models.py
@@ -4,4 +4,4 @@ from django.conf import settings
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     address = models.CharField(max_length=100)
-    zipcode = models.CharField(max_length=6) #, validators=[])
+    zipcode = models.CharField(max_length=6) #, validators=[]) 
