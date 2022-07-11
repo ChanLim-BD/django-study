d09f285b9c1d2f617c8bd17c291d16d03c358c6b
Chan <linsay070@gmail.com>
2022-07-11 14:58:46 +0900

    API 구현 방법은 아직은 필요없다고 판단.

--- django_rest/django_rest/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('api-auth/', include('rest_framework.urls')),
-    path('', include('instagram.urls')),
-    path('admin/', admin.site.urls),
-]
